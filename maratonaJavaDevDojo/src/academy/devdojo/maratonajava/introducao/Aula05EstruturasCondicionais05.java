e7bebba86adfd4aa70a7b602f6c4ed78b8c8fca9
natanael <natanael@usuario-OptiPlex-3080>
2024-04-08 16:25:36 -0300

    Dia 08/04/2024, parei na aula 25, assunto: Switch case.

---
+++ maratonaJavaDevDojo/src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais05.java
@@ -0,0 +1,44 @@
+package academy.devdojo.maratonajava.introducao;
+
+import java.util.Scanner;
+
+public class Aula05EstruturasCondicionais05 {
+
+	public static void main(String[] args) {
+		// Imprima o dia da semana, considerando 1 como domingo, utilizando o switch
+		// case.
+		System.out.println("Digite um dia da semana: ");
+		Scanner teclado = new Scanner (System.in);
+		int diaSemana = teclado.nextInt();
+		teclado.close();
+
+		switch (diaSemana) {
+		case 1:
+			System.out.println("Domingo");
+			break;
+		case 2:
+			System.out.println("Segunda");
+			break;
+		case 3:
+			System.out.println("Terça");
+			break;
+		case 4:
+			System.out.println("Quarta");
+			break;
+		case 5:
+			System.out.println("Quinta");
+			break;
+		case 6:
+			System.out.println("Sexta");
+			break;
+		case 7:
+			System.out.println("Sábado");
+		default:
+			System.out.println("Digite um dia válido!");
+			break;
+			
+		}
+
+	}
+
+}
