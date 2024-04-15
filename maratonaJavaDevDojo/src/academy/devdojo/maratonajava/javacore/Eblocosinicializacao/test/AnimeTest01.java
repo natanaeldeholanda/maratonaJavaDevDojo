893b8add0c22c64e8187426fc55045ff0b06cf47
natanaeldeholanda <natan.holanda@gmail.com>
2024-04-14 22:23:35 -0300

    Assisti a aula 60

--- maratonaJavaDevDojo/src/academy/devdojo/maratonajava/javacore/Eblocosinicializacao/test/AnimeTest01.java
+++ maratonaJavaDevDojo/src/academy/devdojo/maratonajava/javacore/Eblocosinicializacao/test/AnimeTest01.java
@@ -5,7 +5,10 @@ import academy.devdojo.maratonajava.javacore.Eblocosinicializacao.dominio.Anime;
 public class AnimeTest01 {
 
 	public static void main(String[] args) {
-		Anime anime = new Anime();
+		Anime anime = new Anime("One Piece");
+		
+		System.out.println(anime.getEpisodios());
+
 	}
 
 }
