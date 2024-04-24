8deb636fbd9dcecf8881f6ef6818e3efd6fb830d
natanaeldeholanda <natan.holanda@gmail.com>
2024-04-24 16:41:58 -0300

    Assisti até a aula 121

---
+++ maratonaJavaDevDojo/src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalDateTimeTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
+public class LocalDateTimeTest01 {
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDate date = LocalDate.parse("2024-04-24");
+        LocalTime time = LocalTime.parse("15:30");
+        System.out.println(date);
+        System.out.println(time);
+        System.out.println(LocalDateTime.MIN);
+        System.out.println(LocalDateTime.MAX);
+        LocalDateTime ldt1 = date.atTime(time);
+        LocalDateTime ldt2 = time.atDate(date);
+        System.out.println(ldt1);
+        System.out.println(ldt2);
+    }
+}
--- maratonaJavaDevDojo/src/academy/devdojo/maratonajava/javacore/Sformatacao/test/SimpleDateFormatTest01.java
+++ maratonaJavaDevDojo/src/academy/devdojo/maratonajava/javacore/Sformatacao/test/SimpleDateFormatTest01.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class SimpleDateFormatTest01 {
     public static void main(String[] args) {
-        String pattern = "yyyy.MM.dd G 'at' HH:mm:ss z";
+        String pattern = "'Fortaleza' yyyy.MM.dd G 'at' HH:mm:ss z";
         SimpleDateFormat sdf = new SimpleDateFormat(pattern);
         System.out.println(sdf.format(new Date()));
     }
