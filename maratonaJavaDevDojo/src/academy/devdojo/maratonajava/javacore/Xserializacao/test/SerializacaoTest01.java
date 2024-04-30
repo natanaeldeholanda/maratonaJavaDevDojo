a41452945ff91b1f9ffeb9bfc2ac6fef55c348c2
natanaeldeholanda <natan.holanda@gmail.com>
2024-04-30 16:22:01 -0300

    Assisti até a aula 159, porém preciso assistir novamente.

---
+++ maratonaJavaDevDojo/src/academy/devdojo/maratonajava/javacore/Xserializacao/test/SerializacaoTest01.java
@@ -0,0 +1,38 @@
+package academy.devdojo.maratonajava.javacore.Xserializacao.test;
+
+import academy.devdojo.maratonajava.javacore.Xserializacao.dominio.Aluno;
+
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SerializacaoTest01 {
+    public static void main(String[] args) {
+        Aluno natanael = new Aluno(1L, "Natanael de Holanda", "141286");
+        // serializar(natanael);
+        deserializar();
+    }
+
+    private static void serializar(Aluno aluno) {
+        Path path = Paths.get("pasta/aluno.ser");
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(path))) {
+            oos.writeObject(aluno);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void deserializar() {
+        Path path = Paths.get("pasta/aluno.ser");
+        try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(path))) {
+            Aluno natanael = (Aluno) ois.readObject();
+            System.out.println(natanael);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
