9f58871e68f539feb76cdd35b26b5e9c3499b9f0
natanaeldeholanda <natan.holanda@gmail.com>
2024-05-16 19:48:06 -0300

    Código do vídeo 243, refatorado e funcionando

--- maratonaJavaDevDojo/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/service/StoreServiceWithDiscount.java
+++ maratonaJavaDevDojo/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/service/StoreServiceWithDiscount.java
@@ -1,30 +1,34 @@
 package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.service;
 
-
 import academy.devdojo.maratonajava.javacore.ZZGconcorrencia.dominio.Discount;
 import academy.devdojo.maratonajava.javacore.ZZGconcorrencia.dominio.Quote;
 
 import java.util.Locale;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 public class StoreServiceWithDiscount {
     //Método preço Síncrono
     public String getPriceSync(String storeName) {
-       double price = priceGenerator();
-        Discount.Code discountCode = Discount.Code.values()[
-                ThreadLocalRandom.current().nextInt(Discount.Code.values().length)
+        double price = priceGenerator();
+        Discount.Code discountCode = Discount.Code.values()[ThreadLocalRandom.current().nextInt(Discount.Code.values().length)
                 ];
-        return String.format(Locale.US, "%s: %.2f: %s", storeName, price, discountCode);
+        return String.format(Locale.US,"%s:%.2f:%s", storeName, price, discountCode);
     }
 
     public String applyDiscount(Quote quote) {
         delay();
-        double discountValue = quote.getPrice() * (100 - quote.getDiscountCode().getPercentage()) / 100;
-        return String.format("'%s' original price: '%.2f'. Applying discount code '%s'. Final price: '%.2f'",
+        double discountValue = quote.getPrice() * (100 - quote.getDiscountCode().getPercentage() / 100);
+        return String.format("'%s' original price: '%.2f'. Applying discount code '%s'. Final price '%.2f'.",
                 quote.getStore(),
                 quote.getPrice(),
-                quote.getDiscountCode(), discountValue);
+                quote.getDiscountCode(),
+                discountValue);
+    }
+
+    public CompletableFuture<Double> getPriceAsyncCompletableFuture(String storeName) {
+        return CompletableFuture.supplyAsync(this::priceGenerator);
     }
 
     private double priceGenerator() {
--- maratonaJavaDevDojo/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/CompletableFutureTest04.java
+++ maratonaJavaDevDojo/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/CompletableFutureTest04.java
@@ -13,9 +13,9 @@ public class CompletableFutureTest04 {
     }
     private static void searchPricesWithDiscount(StoreServiceWithDiscount service) {
         List<String> stores = List.of("Store 1", "Store 2", "Store 3", "Store 4");
-        //stores.forEach(s -> System.out.println(service.getPriceSync(s)));
+       //stores.forEach(s -> System.out.println(service.getPriceSync(s)));
         stores.stream()
-                .map(service::getPriceSync)//store:price:code
+                .map(service::getPriceSync) //store:price:quote
                 .map(Quote::newQuote)
                 .map(service::applyDiscount)
                 .forEach(System.out::println);
